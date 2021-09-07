0e024b2714755bf98dea4bc12a6253a64ea5cc2f
Chayanit <chayanit.chaisri@gmail.com>
2021-09-07 19:32:49 -0400

    Adding Search bar

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
-import './App.css';
-import React, { useState, useEffect } from 'react';
-import axios from 'axios'
+import "./App.css";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
-
-  const [coins, setCoins] = useState([])
+  const [coins, setCoins] = useState([]);
   useEffect(() => {
-    axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false')
-    .then(res => setCoins(res.data))
-    .catch(error => console.log(error))
-  }, [])
+    axios
+      .get(
+        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false"
+      )
+      .then((res) => setCoins(res.data))
+      .catch((error) => console.log(error));
+  }, []);
   return (
-    <div className="App">
-      <h1>React Crypto Tracker</h1>
+    <div className="coin-app">
+      <div className='coin-search'>
+        <h1 className='coin-text'>Search a currency</h1>
+        <form>
+          <input type='text' placeholder='Search' classNamer='coin-input' />
+        </form>
+      </div>
     </div>
   );
 }
