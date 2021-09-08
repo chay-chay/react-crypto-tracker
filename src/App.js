8fbaf7b54cd4907e619bdfabb491ea1f844e71cf
Chayanit <chayanit.chaisri@gmail.com>
2021-09-07 20:20:37 -0400

    Adding filteredCoins & pass props to Coin component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import "./App.css";
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import Coin from './Coin'
 
 function App() {
   const [coins, setCoins] = useState([]);
+  const [search, setSearch] = useState('')
+
   useEffect(() => {
     axios
       .get(
@@ -12,14 +15,30 @@ function App() {
       .then((res) => setCoins(res.data))
       .catch((error) => console.log(error));
   }, []);
+  
+  const handleChange = e => {
+    console.log(e.target.value)
+  }
+
+ const filteredCoins = coins.filter(coin =>
+    coin.name.toLowerCase().includes(search.toLowerCase())
+  );
+ 
+
   return (
     <div className="coin-app">
       <div className='coin-search'>
         <h1 className='coin-text'>Search a currency</h1>
         <form>
-          <input type='text' placeholder='Search' classNamer='coin-input' />
+          <input type='text' placeholder='Search' className='coin-input' onChange={handleChange} />
         </form>
       </div>
+      {filteredCoins.map(coin => {
+        console.log(coin.name)
+        return (
+          <Coin key={coin.id} name={coin.name} image={coin.image} symbol={coin.symbol} price={coin.current_price} volume={coin.market_cap} />
+        )
+      })}
     </div>
   );
 }
